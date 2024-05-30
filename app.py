3520913446b4ee86a139fe00bdf715fa7da8d415
Riyanna Maria Abison <98329821+Riyanna2233@users.noreply.github.com>
2024-05-30 11:31:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-# app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 import tensorflow as tf
 import joblib
@@ -9,6 +8,10 @@ app = Flask(__name__)
 # Load the model and scaler
 model = tf.keras.models.load_model('lstm_model.h5')
 scaler = joblib.load('scaler.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -27,5 +30,4 @@ def predict():
         return jsonify({'error': 'Input data is too short'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)  # Change 8000 to the port number you want to use
-
+    app.run(debug=True, port=4000)
